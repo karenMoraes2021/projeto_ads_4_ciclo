4dc067d367ed96573768677c2fb4e69f85754107
karenMoraes2021 <kantora16@hotmail.com>
2021-05-06 21:04:51 -0300

    Update t_sobrePage.dart

--- lib/t_sobrePage.dart
+++ lib/t_sobrePage.dart
@@ -15,58 +15,62 @@ class Sobre extends StatelessWidget {
         title: const Text('Sobre'),
       ),
       body: ListView(
-        padding: EdgeInsets.all(10),        
+        padding: EdgeInsets.all(10),
         children: [
-        Column(
-          children: [
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(5)),
-                Text(
-                  'Sobre o App',
-                  style: Theme.of(context).textTheme.headline4,
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(10)),
-                Expanded(child: Text('TEMA: Aplicativo para um Terreiro de Umbanda, gerenciando todas as funções administrativas')),                   
-              ],
-            ),
-             Row(
-              children: [
-                Padding(padding: EdgeInsets.all(10)),
-                Expanded(child: Text('')),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(10)),
-                Expanded(child: Text('OBJETIVO: Ajudar no auxilio dos Médiuns sobre os dias das giras bem como os recados da semana e controle do estoque')),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(10)),
-                Expanded(child: Text('')),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(5)),
-                Text(
-                  'Desenvolvedores',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ],
-            ),
-            Row(children: [
-              Flexible(child: desenvolvedor1(context)),
-              Flexible(child: desenvolvedor2(context)),
-            ])
-          ],
-        ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(5)),
+                  Text(
+                    'Sobre o App',
+                    style: Theme.of(context).textTheme.headline4,
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(10)),
+                  Expanded(
+                      child: Text(
+                          'TEMA: Aplicativo para um Terreiro de Umbanda, gerenciando todas as funções administrativas')),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(10)),
+                  Expanded(child: Text('')),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(10)),
+                  Expanded(
+                      child: Text(
+                          'OBJETIVO: Ajudar no auxilio dos Médiuns sobre os dias das giras bem como os recados da semana e controle do estoque')),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(10)),
+                  Expanded(child: Text('')),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(5)),
+                  Text(
+                    'Desenvolvedores',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ],
+              ),
+              Row(children: [
+                Flexible(child: desenvolvedor1(context)),
+                Flexible(child: desenvolvedor2(context)),
+              ])
+            ],
+          ),
         ],
       ),
       //backgroundColor: Colors.grey,
@@ -93,16 +97,18 @@ Widget desenvolvedor1(context) {
             alignment: Alignment.center,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(50),
-              child: Image.asset('lib/images/Karen.jpg', width: 200, height: 200),
+              child:
+                  Image.asset('lib/images/Karen.jpg', width: 200, height: 200),
             ),
           ),
           Row(children: [
             Text('Karen Moraes', style: Theme.of(context).textTheme.headline6)
           ]), //Nome
-          Row(
-            children: [
-              Expanded(child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis libero quis nibh tincidunt.'))]
-          ), // Outra coisa
+          Row(children: [
+            Expanded(
+                child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis libero quis nibh tincidunt.'))
+          ]), // Outra coisa
         ],
       ));
 }
@@ -114,7 +120,7 @@ Widget desenvolvedor2(context) {
       //width: 200,
       height: 500,
       decoration: BoxDecoration(
-        color: Colors.grey[100],  
+        color: Colors.grey[100],
         border: Border.all(),
       ),
       child: Column(
@@ -122,21 +128,21 @@ Widget desenvolvedor2(context) {
         mainAxisSize: MainAxisSize.min,
         children: [
           Container(
-            padding: EdgeInsets.all(20),
-            alignment: Alignment.center,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50),
-              child: Image.asset('lib/images/perfil1.jpeg',width: 200, height: 200)
-            )
-          ),
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.center,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.asset('lib/images/perfil1.jpeg',
+                      width: 200, height: 200))),
           Row(children: [
             Text('Alessandro Pereira',
                 style: Theme.of(context).textTheme.headline6)
           ]), // Nome
-          Row(
-            children: [
-              Expanded(child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis libero quis nibh tincidunt.'))]
-          ), // Outra coisa
+          Row(children: [
+            Expanded(
+                child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis libero quis nibh tincidunt.'))
+          ]), // Outra coisa
         ],
       ));
 }
